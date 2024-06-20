aac5123e2ffd0aa6144ae004fcbe35aa407b456e
MaazDanish <dk599318@gmail.com>
2024-06-20 16:44:52 +0530

    Multiple Props

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
     <div>
       <h> Hello world</h>
       <ExpenseItem date={new Date(2024, 6, 21)} title='Book' price='20' location='Delhi'></ExpenseItem>
+      <ExpenseItem date={new Date(2024, 7, 21)} title='phone' price='20000' location='Mumbau'></ExpenseItem>
+      <ExpenseItem date={new Date(2024, 8, 21)} title='mobile' price='2000000' location='SRe'></ExpenseItem>
+      <ExpenseItem date={new Date(2024, 9, 21)} title='apple' price='200' location='behat'></ExpenseItem>
     </div>
   );
 }
