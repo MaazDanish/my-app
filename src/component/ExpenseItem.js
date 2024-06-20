18119d538415d629fd7457611356dde34c29aa88
MaazDanish <dk599318@gmail.com>
2024-06-20 22:10:20 +0530

    Splitting the components

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,19 +1,9 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
 function ExpenseItem(props) {
-
-    const months = ['January', 'februry', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-    const month = months[props.date.getMonth()];
-    // console.log(month,"Month is here");
-    const year = props.date.getFullYear();
-    const day = props.date.getDate();
-
     return (
         <div className="expense-item">
-            <div >
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div>
+            <ExpenseDate date={props.date}></ExpenseDate>
             <div className='expense-item__location '>{props.location}</div>
             <div className='expense-item__description'>
                 <h1>{props.title}</h1>
