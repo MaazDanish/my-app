6cdac03100d4ae0cd510dfe448f61ae385e1af1e
MaazDanish <dk599318@gmail.com>
2024-06-20 22:15:08 +0530

    Alternative way to write custom component

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -3,7 +3,7 @@ import ExpenseDate from './ExpenseDate';
 function ExpenseItem(props) {
     return (
         <div className="expense-item">
-            <ExpenseDate date={props.date}></ExpenseDate>
+            <ExpenseDate date={props.date} />
             <div className='expense-item__location '>{props.location}</div>
             <div className='expense-item__description'>
                 <h1>{props.title}</h1>
