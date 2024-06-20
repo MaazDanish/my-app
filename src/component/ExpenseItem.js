a7f92e2651d2087e736bde489685fad3d3441efa
MaazDanish <dk599318@gmail.com>
2024-06-20 16:07:36 +0530

    Applied Css to the component

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,13 +1,14 @@
+import './ExpenseItem.css'
 function ExpenseItem() {
     return (
-        <>
-            <div>15 August 2024</div>
-            <div>Delhi</div>
-            <div>
+        <div className="expense-item">
+            <div >15 August 2024</div>
+            <div className='expense-item__location '>Delhi</div>
+            <div className='expense-item__description'>
                 <h1>Book</h1>
-                <div>Rs. 500</div>
+                <div className='expense-item__price'>Rs. 500</div>
             </div>
-        </>
+        </div>
     )
 }
 
