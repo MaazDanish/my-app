a552b6cb6eb85e8eb025ca530f8c4b31dd7ea745
MaazDanish <dk599318@gmail.com>
2024-06-20 21:21:16 +0530

    Extract date from date Object

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,8 +1,19 @@
 import './ExpenseItem.css'
 function ExpenseItem(props) {
+
+    const months = ['January', 'februry', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    const month = props.date.getMonth();
+    // console.log(month,"Month is here");
+    const year = props.date.getFullYear();
+    const day = props.date.getDate();
+
     return (
         <div className="expense-item">
-            <div >{props.date.toISOString()}</div>
+            <div >
+                <div>{months[month]}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <div className='expense-item__location '>{props.location}</div>
             <div className='expense-item__description'>
                 <h1>{props.title}</h1>
